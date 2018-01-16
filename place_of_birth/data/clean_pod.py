f621c87f59cf0dc73df41034a5a5d9ef9781024d
Jesse Mu <jlm95@cam.ac.uk>
2018-01-16 22:25:20 +0000

    Add place of death datasets

---
+++ place_of_birth/data/clean_pod.py
@@ -0,0 +1,74 @@
+"""
+Get confirmed (i.e. yes vote) place-of-death relations from Google.
+"""
+
+import json
+import requests
+from urllib import parse
+import tqdm
+
+
+with open('.api_key', 'r') as f:
+    API_KEY = f.read().strip()
+
+
+def get_name(mid):
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+        'ids': mid,
+        'limit': 1,
+        'key': API_KEY
+    }
+    url = service_url + '?' + parse.urlencode(params)
+    r = requests.get(url).json()
+    if len(r['itemListElement']) != 1:
+        return None
+    try:
+        return r['itemListElement'][0]['result']['name']
+    except Exception:
+        return None
+
+
+if __name__ == "__main__":
+    total = 0
+    hits = 0
+    with open('pod.tsv', 'w') as fout, \
+            open('pod.json', 'w') as jsonout, \
+            open('pod_gold.tsv', 'w') as goldf, \
+            open('20131104-place_of_death.json', 'r') as fin:
+        lines_i = list(enumerate(fin.readlines()))
+        for i, line in tqdm.tqdm(lines_i):
+            total += 1
+            dat = json.loads(line)
+
+            judgments = [x['judgment'] == 'yes'
+                         for x in dat['judgments']]
+
+            overall = (sum(judgments) / len(judgments)) > 0.5
+            if not overall:
+                continue
+
+            sub_mid = dat['sub']
+            obj_mid = dat['obj']
+            # Get api call
+            sub_name = get_name(sub_mid)
+            obj_name = get_name(obj_mid)
+            if sub_name is None or obj_name is None:
+                print("Skipping")
+                continue
+            hits += 1
+            if len(dat['evidences']) != 1:
+                raise Exception(dat)
+            dat = {
+                'text': dat['evidences'][0]['snippet'],
+                'sub': sub_name,
+                'obj': obj_name,
+                'n_raters': len(judgments),
+                'rater_percent': sum(judgments) / len(judgments),
+                'overall': overall,
+            }
+            fout.write('{}\t{}\n'.format('d' + str(i), dat['text']))
+            jsonout.write('{}\n'.format(json.dumps(dat)))
+            goldf.write('{}\t{}\t{}\t{}\n'.format('d' + str(i), dat['sub'],
+                                                  dat['obj'], dat['overall']))
+    print(total, hits)
--- place_of_birth/data/resolve_pod_labels.py
+++ place_of_birth/data/resolve_pod_labels.py
@@ -8,6 +8,7 @@ def fix_person(person, text):
         if newp in text:
             #  print("Removing middle name worked")
             return newp
+        # Try last name only
         if ptokens[2] in text:
             return ptokens[2]
     if len(ptokens) == 2:
@@ -24,8 +25,8 @@ def fix_person(person, text):
     return None
 
 
-def fix_pob(pob, text):
-    commatokens = pob.split(', ')
+def fix_pod(pod, text):
+    commatokens = pod.split(', ')
     if len(commatokens) == 2:
         # city, state. Try just city. Then try just state
         if commatokens[0] in text:
@@ -34,35 +35,34 @@ def fix_pob(pob, text):
 
 
 if __name__ == "__main__":
-    docs = pd.read_csv('pob.tsv', sep='\t', header=None,
+    docs = pd.read_csv('pod.tsv', sep='\t', header=None,
                        names=['docid', 'text'])
-    labels = pd.read_csv('pob_gold.tsv', sep='\t', header=None,
-                         names=['docid', 'person', 'pob', 'label'])
+    labels = pd.read_csv('pod_gold.tsv', sep='\t', header=None,
+                         names=['docid', 'person', 'pod', 'label'])
 
     correct_labels = []
 
     combined = docs.merge(labels, on='docid')
     hits = 0
     person_misses = 0
-    pob_misses = 0
+    pod_misses = 0
     for _, row in combined.iterrows():
         docid = row['docid']
         person = row['person']
-        pob = row['pob']
+        pod = row['pod']
         text = row['text']
 
-        # Simple fixes if person/pob not in text.
+        # Simple fixes if person/pod not in text.
         if person not in text:
             person = fix_person(person, text)
             if person is None:
                 person_misses += 1
                 continue
 
-        if pob not in text:
-            pob = fix_pob(pob, text)
-            if pob is None:
-                #  print("{} missing:{}".format(row['pob'], row['text']))
-                pob_misses += 1
+        if pod not in text:
+            pod = fix_pod(pod, text)
+            if pod is None:
+                pod_misses += 1
                 continue
 
         hits += 1
@@ -70,20 +70,20 @@ if __name__ == "__main__":
         # XXX: snorkel spans don't use python slicing (i.e. up to end of range)
         # - so do minus 1 here
         person_end = person_start + len(person) - 1
-        pob_start = text.index(pob)
-        pob_end = pob_start + len(pob) - 1
+        pod_start = text.index(pod)
+        pod_end = pod_start + len(pod) - 1
 
         # Get stable ids
         person_sid = '{}::span:{}:{}'.format(docid, person_start, person_end)
-        pob_sid = '{}::span:{}:{}'.format(docid, pob_start, pob_end)
+        pod_sid = '{}::span:{}:{}'.format(docid, pod_start, pod_end)
 
         correct_labels.append(
-            (person_sid, pob_sid, row['label']))
+            (person_sid, pod_sid, row['label']))
 
     print("Total", combined.shape[0], "after filtering", hits)
     print("Person misses", person_misses)
-    print("POB misses", pob_misses)
+    print("pod misses", pod_misses)
     correct_df = pd.DataFrame.from_records(
-        correct_labels, columns=['person', 'pob', 'label'])
-    correct_df.to_csv('pob_gold_span.tsv', sep='\t', header=True,
+        correct_labels, columns=['person', 'pod', 'label'])
+    correct_df.to_csv('pod_gold_span.tsv', sep='\t', header=True,
                       index=False)
